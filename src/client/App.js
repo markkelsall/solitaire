40c1300c0d9bab2fd6dcd57fa3f3c1a13045fc17
Mark Kelsall <mark.kelsall@gmail.com>
2019-10-13 17:16:30 +0100

    feat: add background colour

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Game from "./components/Game";
 import Tableau from "./components/Tableau";
 
 const App = () => {
   return (
-    <div>
+      <Game>
         <Tableau />
-    </div>
+      </Game>
   );
 }
 
---
+++ src/client/components/Game/Game.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './style.css';
+
+const Game = ({ children }) => {
+  return (<div className='game'>{children}</div>);
+}
+
+export default Game;
