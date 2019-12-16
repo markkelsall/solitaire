580e0b3a58e554ce8a2e987247ee7e751b4135d7
Mark Kelsall <mark.kelsall@gmail.com>
2019-12-16 20:19:51 +0000

    fix: stop beautify running

--- src/client/components/Pile/Pile.js
+++ src/client/components/Pile/Pile.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Pile = () => {
-  return ( <
-    div className = 'w-32 h-48 border-dashed border-2 border-black rounded-lg' > < /div>
-  );
+    return (<div className='w-32 h-48 border-dashed border-2 border-black rounded-lg' > </div>);
 }
 
 export default Pile;
