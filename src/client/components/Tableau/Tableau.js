f92f992daedeb673b78a94fd279ba6c0abb11ee2
Mark Kelsall <mark.kelsall@gmail.com>
2019-12-17 15:27:24 +0000

    fix: format code

--- src/client/components/Tableau/Tableau.js
+++ src/client/components/Tableau/Tableau.js
@@ -2,45 +2,30 @@ import React from "react";
 import Pile from "../Pile/Pile";
 
 const Tableau = () => {
-  return ( <
-    div className = 'flex flex-row' >
-    <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> <
-    div className = 'mr-4' >
-    <
-    Pile / >
-    <
-    /div> < /
-    div >
+  return (
+    <div className='flex flex-row'>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+      <div className='mr-4'>
+        <Pile />
+      </div>
+    </div>
   );
 }
 
